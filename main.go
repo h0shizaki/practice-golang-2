ac48b593d73c2bfa48d9067498831d2a2cfe46db
Kitsune-fx <kant.cmd@gmail.com>
2021-11-26 22:31:10 +0700

    [Connect to DB]

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	fmt.Println("Hello GO")
+	fmt.Println("Driver : ", sql.Drivers())
+
+	db, err := sql.Open("mysql", "root:@/test")
+
+	if err != nil {
+		fmt.Println("Can not connect to Database ")
+	}
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		fmt.Println("Error while fectching data")
+	}
+
+	for results.Next() {
+		var (
+			id     int
+			f_name string
+			l_name string
+		)
+		err = results.Scan(&id, &f_name, &l_name)
+		if err != nil {
+			fmt.Println("Unable to parse row")
+		}
+
+		fmt.Printf("ID : %d Name: %s %s \n", id, f_name, l_name)
+	}
+
+}
